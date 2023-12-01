2dc7bd67e44c96a52074dac604c4b06c70539e65
SREERAJ <admin@SREERAJ-S-R-MacBook-Air.local>
2023-12-01 21:32:44 +0530

    signup page frontend validation completed

--- client/src/app/components/user/signup/signup.component.ts
+++ client/src/app/components/user/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,6 +8,26 @@ import { Component } from '@angular/core';
 })
 export class SignupComponent {
   hide = true;
+  hide1=true
+  passError?:boolean
+  signupFormControl:FormGroup = new FormGroup({
+    firstname:new FormControl(null,[Validators.required]),
+    lastname:new FormControl(null,Validators.required),
+    email : new FormControl(null,[Validators.required,Validators.email]),
+    mobile:new FormControl(null,[Validators.required,Validators.minLength(10)]),
+    gender:new FormControl('Male',[Validators.required]),
+    password:new FormControl(null,[Validators.required,Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#]).{8,}$')]),
+    renteredpassword : new FormControl(null,[Validators.required,Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#]).{8,}$')])
+  })
 
 
+  onSignUpFormSubmit(){
+  if(this.signupFormControl.get('password')?.value!==this.signupFormControl.get('renteredpassword')?.value ){
+    this.passError=true
+
+    console.log(this.signupFormControl);
+  }
+
+
+  }
 }
